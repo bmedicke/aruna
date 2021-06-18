8190f2a94ba06b8cebfd0cb68b2342f7bd4a23db
Benjamin Medicke <bmedicke@gmail.com>
2021-06-18 10:14:36 +0200

    move Box to seperate file

--- src/App.js
+++ src/App.js
@@ -1,38 +1,8 @@
-import React, { useRef, useState } from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
+import React from 'react'
+import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
+import Box from './components/Box.js'
 import './App.css'
-
-function Box (props) {
-  const mesh = useRef()
-  const [hovered, setHover] = useState(false)
-  const [active, setActive] = useState(false)
-
-  useFrame(() => (mesh.current.rotation.x += 0.01))
-
-  return (
-    <mesh
-      {...props}
-      ref={mesh}
-      scale={active ? 1.5 : 1}
-      onClick={(event) => {
-        setActive(!active)
-        console.log(event)
-      }}
-      onPointerOver={(event) => {
-        setHover(true)
-        console.log(event)
-      }}
-      onPointerOut={(event) => {
-        setHover(false)
-        console.log(event)
-      }}
-    >
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? 'red' : 'grey'} />
-    </mesh>
-  )
-}
 
 function App () {
   return (
---
+++ src/components/Box.js
@@ -0,0 +1,35 @@
+import React, { useRef, useState } from 'react'
+import { useFrame } from '@react-three/fiber'
+
+function Box (props) {
+  const mesh = useRef()
+  const [hovered, setHover] = useState(false)
+  const [active, setActive] = useState(false)
+
+  useFrame(() => (mesh.current.rotation.x += 0.01))
+
+  return (
+    <mesh
+      {...props}
+      ref={mesh}
+      scale={active ? 1.5 : 1}
+      onClick={(event) => {
+        setActive(!active)
+        console.log(event)
+      }}
+      onPointerOver={(event) => {
+        setHover(true)
+        console.log(event)
+      }}
+      onPointerOut={(event) => {
+        setHover(false)
+        console.log(event)
+      }}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? 'red' : 'grey'} />
+    </mesh>
+  )
+}
+
+export default Box
