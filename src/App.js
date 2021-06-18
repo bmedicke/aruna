41c0df3375da4f0fa6eaf04ab575454a366a67fe
Benjamin Medicke <bmedicke@gmail.com>
2021-06-18 09:47:31 +0200

    add boxes, add lights, add orbitcontrols

--- src/App.js
+++ src/App.js
@@ -1,11 +1,53 @@
-import React from 'react'
-import { Canvas } from '@react-three/fiber'
+import React, { useRef, useState } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
 import './App.css'
+
+function Box (props) {
+  const mesh = useRef()
+  const [hovered, setHover] = useState(false)
+  const [active, setActive] = useState(false)
+
+  useFrame(() => (mesh.current.rotation.x += 0.01))
+
+  return (
+    <mesh
+      {...props}
+      ref={mesh}
+      scale={active ? 1.5 : 1}
+      onClick={(event) => {
+        setActive(!active)
+        console.log(event)
+      }}
+      onPointerOver={(event) => {
+        setHover(true)
+        console.log(event)
+      }}
+      onPointerOut={(event) => {
+        setHover(false)
+        console.log(event)
+      }}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? 'red' : 'grey'} />
+    </mesh>
+  )
+}
 
 function App () {
   return (
     <div className='App'>
-      <Canvas />
+      <Canvas>
+        <ambientLight />
+        <pointLight position={[0, 0, 3]} />
+
+        <OrbitControls />
+
+        <Box position={[1, 1, 0]} />
+        <Box position={[-1, 1, 0]} />
+        <Box position={[-1, -1, 0]} />
+        <Box position={[1, -1, 0]} />
+      </Canvas>
     </div>
   )
 }
