82bffaf2fc2c963f408a3a26f45a735346816719
Benjamin Medicke <bmedicke@gmail.com>
2021-06-18 13:58:17 +0200

    add hover state and handlers

--- src/components/Model.js
+++ src/components/Model.js
@@ -1,20 +1,29 @@
 /* eslint-disable react/prop-types */
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import { STLLoader } from 'three/examples/jsm/loaders/STLLoader'
 import { useLoader } from '@react-three/fiber'
 
 function Model ({ url }, props) {
   const mesh = useRef()
   const geom = useLoader(STLLoader, url)
+  const [hovered, setHover] = useState(false)
 
   return (
     <mesh
+      {...props}
       ref={mesh}
       position={[-120, -110, 0]}
-      {...props}
+      onPointerOver={(event) => {
+        setHover(true)
+        console.log(event)
+      }}
+      onPointerOut={(event) => {
+        setHover(false)
+        console.log(event)
+      }}
     >
       <primitive object={geom} attach='geometry' />
-      <meshStandardMaterial color='grey' />
+      <meshStandardMaterial color={hovered ? 'red' : 'grey'} />
     </mesh>
   )
 }
