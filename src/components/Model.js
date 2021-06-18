42455001b00b41f19a9d0c18b769aaf69e0a623f
Benjamin Medicke <bmedicke@gmail.com>
2021-06-18 14:15:02 +0200

    rotate if clicked on

--- src/components/Model.js
+++ src/components/Model.js
@@ -1,19 +1,30 @@
 /* eslint-disable react/prop-types */
 import React, { useRef, useState } from 'react'
 import { STLLoader } from 'three/examples/jsm/loaders/STLLoader'
-import { useLoader } from '@react-three/fiber'
+import { useLoader, useFrame } from '@react-three/fiber'
 
 function Model ({ url }, props) {
   const mesh = useRef()
   const geom = useLoader(STLLoader, url)
   geom.center()
   const [hovered, setHover] = useState(false)
+  const [active, setActive] = useState(false)
+
+  useFrame(() => {
+    if (active) {
+      mesh.current.rotation.y += 0.01
+    }
+  })
 
   return (
     <mesh
       {...props}
       ref={mesh}
       position={[0, 0, 0]}
+      onClick={(event) => {
+        setActive(!active)
+        console.log(event)
+      }}
       onPointerOver={(event) => {
         setHover(true)
         console.log(event)
