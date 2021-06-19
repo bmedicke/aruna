6540f8139fa432c6cde99d9159d96bf0aea37855
Niko Benedikt <benediktnikolai@gmail.com>
2021-06-19 04:03:48 +0200

    add layout component

--- src/App.js
+++ src/App.js
@@ -4,39 +4,42 @@ import Model from './components/Model.js'
 import React, { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
 import { MapControls, Text } from '@react-three/drei'
+import Layout from './HOC/Layout'
 
 function App () {
   return (
-    <div className='App'>
-      <Canvas
-        orthographic
-        camera={{ zoom: 10, position: [0, 0, 200] }}
-        style={{ backgroundColor: 'black' }}
-      >
-        <ambientLight intensity={0.3} />
-        <pointLight position={[0, 0, 3]} />
-
-        <MapControls />
-
-        <Text
-          color='white'
-          anchorX='center'
-          anchorY='middle'
-          fontSize={10}
-          position={[0, 30, 0]}
+    <Layout>
+      <div className="App">
+        <Canvas
+          orthographic
+          camera={{ zoom: 10, position: [0, 0, 200] }}
+          style={{ backgroundColor: 'black' }}
         >
-          Aruna
-        </Text>
+          <ambientLight intensity={0.3} />
+          <pointLight position={[0, 0, 3]} />
 
-        {[...Array(500)].map((x, i) =>
-          <Dot key={i} position={[i * 0.2 - 100 / 2, -10, -20]} />
-        )}
+          <MapControls />
 
-        <Suspense fallback={null}>
-          <Model url='./models/hexlamp.stl' />
-        </Suspense>
-      </Canvas>
-    </div>
+          <Text
+            color="white"
+            anchorX="center"
+            anchorY="middle"
+            fontSize={10}
+            position={[0, 30, 0]}
+          >
+            Aruna
+          </Text>
+
+          {[...Array(500)].map((x, i) => (
+            <Dot key={i} position={[i * 0.2 - 100 / 2, -10, -20]} />
+          ))}
+
+          <Suspense fallback={null}>
+            <Model url="./models/hexlamp.stl" />
+          </Suspense>
+        </Canvas>
+      </div>
+    </Layout>
   )
 }
 
