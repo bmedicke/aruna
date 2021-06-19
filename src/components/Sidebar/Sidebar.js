b3e9573acf2f26bafb6ab2ed0af21eb00bf41365
Niko Benedikt <benediktnikolai@gmail.com>
2021-06-19 04:17:39 +0200

    add sidebar component

---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,28 @@
+import { React, useState } from 'react'
+import SidebarButton from './SidebarButton/SidebarButton'
+import './Sidebar.scss'
+
+const Sidebar = () => {
+  const [isClosed, setIsClosed] = useState(true)
+
+  let lis = ['1', '2', '3', '4']
+
+  let clickHandler = () => {
+    setIsClosed(!isClosed)
+    console.log('clicked')
+  }
+
+  return (
+    <div className={`sidebar-wrapper${isClosed ? ' closed' : ''}`}>
+      <div className="sidebar">
+        <p className="sidebar__logo" onClick={() => clickHandler()}>
+          Aruna
+        </p>
+        {lis.map((li, index) => (
+          <SidebarButton key={index} text={li} click={() => clickHandler()} />
+        ))}
+      </div>
+    </div>
+  )
+}
+export default Sidebar
