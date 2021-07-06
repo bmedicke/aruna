726f5d112b257ee2a19563f1f99bcc79aec993d5
Benjamin Medicke <bmedicke@gmail.com>
2021-07-06 14:21:40 +0200

    keep connection

--- backend/demo_rbg.py
+++ backend/demo_rbg.py
@@ -4,24 +4,24 @@ import psycopg2
 import time
 
 
-def update_pixel(id, color):
-    conn = psycopg2.connect(
-        host="localhost",
-        database="postgres",
-        user="postgres",
-        password="postgres",
-    )
-    cur = conn.cursor()
-
+def update_pixel(conn, cur, id, color):
     sql = f"update pixels set red={color[0]}, green={color[1]}, blue={color[2]} where id = {id}"
     cur.execute(sql)
     conn.commit()
 
 
+conn = psycopg2.connect(
+    host="localhost",
+    database="postgres",
+    user="postgres",
+    password="postgres",
+)
+cur = conn.cursor()
+
 for i in range(int(input("how many leds to control? "))):
     if i % 3 == 0:
-        update_pixel(i, (20, 0, 0))
+        update_pixel(conn, cur, i, (20, 0, 0))
     elif i % 3 == 1:
-        update_pixel(i, (0, 20, 0))
+        update_pixel(conn, cur, i, (0, 20, 0))
     else:
-        update_pixel(i, (0, 0, 20))
+        update_pixel(conn, cur, i, (0, 0, 20))
