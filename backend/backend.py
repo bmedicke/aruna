19d1fa445b76d9d28f797136586c122dac488c75
Ben <bmedicke@gmail.com>
2021-11-22 21:45:33 +0100

    use with as

--- backend/backend.py
+++ backend/backend.py
@@ -4,19 +4,19 @@ import board
 import neopixel
 import psycopg
 
-connection = psycopg.connect(
+with psycopg.connect(
     "postgresql://postgres:postgres@localhost/postgres"
-)
+) as connection:
 
-sql_query = """
-SELECT table_schema || '.' || table_name
-FROM information_schema.tables
-WHERE table_type = 'BASE TABLE'
-AND table_schema NOT IN ('pg_catalog', 'information_schema');
-"""
+    sql_query = """
+    SELECT table_schema || '.' || table_name
+    FROM information_schema.tables
+    WHERE table_type = 'BASE TABLE'
+    AND table_schema NOT IN ('pg_catalog', 'information_schema');
+    """
 
-cursor = connection.cursor()
-cursor.execute(sql_query)
+    cursor = connection.cursor()
+    cursor.execute(sql_query)
 
-for result in cursor.fetchall():
-    print(result)
+    for result in cursor.fetchall():
+        print(result)
