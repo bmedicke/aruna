c7fa2799f2bf0331b5bb8edfd36e22c26f80989c
Ben <bmedicke@gmail.com>
2021-11-22 21:46:37 +0100

    move sql out of the way

--- backend/backend.py
+++ backend/backend.py
@@ -4,16 +4,16 @@ import board
 import neopixel
 import psycopg
 
+sql_query = """
+SELECT table_schema || '.' || table_name
+FROM information_schema.tables
+WHERE table_type = 'BASE TABLE'
+AND table_schema NOT IN ('pg_catalog', 'information_schema');
+"""
+
 with psycopg.connect(
     "postgresql://postgres:postgres@localhost/postgres"
 ) as connection:
-
-    sql_query = """
-    SELECT table_schema || '.' || table_name
-    FROM information_schema.tables
-    WHERE table_type = 'BASE TABLE'
-    AND table_schema NOT IN ('pg_catalog', 'information_schema');
-    """
 
     cursor = connection.cursor()
     cursor.execute(sql_query)
