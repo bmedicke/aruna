8543053a5121c5833bdc4175f8226138363d0718
Ben <bmedicke@gmail.com>
2021-11-22 20:58:00 +0100

    add db connection

--- backend/backend.py
+++ backend/backend.py
@@ -4,3 +4,22 @@ import board
 import neopixel
 import psycopg2
 
+conn = psycopg2.connect(
+    host="localhost",
+    database="postgres",
+    user="postgres",
+    password="postgres",
+)
+
+sql = """
+SELECT table_schema || '.' || table_name
+FROM information_schema.tables
+WHERE table_type = 'BASE TABLE'
+AND table_schema NOT IN ('pg_catalog', 'information_schema');
+"""
+
+cur = conn.cursor()
+cur.execute(sql)
+
+for result in cur.fetchall():
+    print(result)
